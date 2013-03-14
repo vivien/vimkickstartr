aa17fddc27ba456844f9c43474b0e07c1c02279a
Vivien Didelot <vivien@didelot.org>
2013-03-14 11:06:24 -0300

    get.sh: account renamed to vivien

--- get.sh
+++ get.sh
@@ -7,8 +7,8 @@ do
   [ -e $i -o -h $i ] && mv $i $BAK
 done
 
-curl -sS -L https://raw.github.com/v0n/vimkickstartr/master/vimrc -o ~/.vimrc
-[ -e ~/.vim-addons ] || curl -sS -L https://raw.github.com/v0n/vimkickstartr/master/lists/default.addons -o ~/.vim-addons
+curl -sS -L https://raw.github.com/vivien/vimkickstartr/master/vimrc -o ~/.vimrc
+[ -e ~/.vim-addons ] || curl -sS -L https://raw.github.com/vivien/vimkickstartr/master/lists/default.addons -o ~/.vim-addons
 
 echo "Backup in $BAK"
 echo "Done."
